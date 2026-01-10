efdbb2da5573f79245f639a670c127ee5736ac47
asjadakhtar <asjadtech203@gmail.com>
2026-01-10 13:35:50 +0500

    #

---
+++ php/custom-fields/blocks/car-gallery.php
@@ -0,0 +1,58 @@
+<section id="fullpage">
+  <div class="section car-section flex flex-col justify-center items-center px-17.5 bg-[#d9d9d9]">
+    <!-- DASHING GROUP -->
+        <div id="group-dashing" class="car-group active-group flex flex-col items-center">
+        <div class="relative flex justify-center items-center">
+            <img src="pics/title(4).png" alt="" class="gsap-reveal absolute bottom-15 z-10 w-236" />
+            <img src="pics/car(4).png" alt="" class="car-move relative z-20 w-225" />
+        </div>
+        <!-- Specific button for Dashing -->
+        <a href="dashing.html " class="gsap-reveal button-reveal mt-9 border-2 border-black text-black text-base font-normal py-4 px-12 md:py-3.5 md:px-31 rounded-sm">
+            Explore
+        </a>
+        </div>
+
+        <!-- X70 PLUS GROUP -->
+        <div id="group-x70" class="car-group flex-col items-center">
+        <div class="relative flex justify-center items-center">
+            <img src="pics/title(5).png" alt="" class="gsap-reveal absolute bottom-15 z-10 w-236" />
+            <img src="pics/car(5).png" alt="" class="car-move relative z-20 w-225" />
+        </div>
+        <!-- Specific button for X70 -->
+        <a href="x70.html" class="gsap-reveal button-reveal mt-9 border-2 border-black text-black text-base font-normal py-4 px-12 md:py-3.5 md:px-31 rounded-sm">
+            Explore 
+        </a>
+        </div>
+
+    <!-- THUMBNAILS -->
+    <div class="flex gap-x-12 pt-15">
+      <div
+        class="car-selector cursor-pointer active-thumb"
+        data-target="group-dashing"
+        data-link="dashing.html"
+      >
+        <img
+          src="pics/car(4).png"
+          alt="DASHING"
+          class="w-44 pb-6 transition-opacity"
+        />
+        <h2 class="flex justify-center text-2xl text-[#6c6c6c]">DASHING</h2>
+      </div>
+
+      <div
+        class="car-selector cursor-pointer opacity-50"
+        data-target="group-x70"
+        data-link="x70.html"
+      >
+        <img
+          src="pics/car(5).png"
+          alt="X70 PLUS"
+          class="w-44 pb-6 transition-opacity"
+        />
+        <h2 class="flex justify-center text-2xl text-[#6c6c6c]">
+          X70 PLUS
+        </h2>
+      </div>
+    </div>
+  </div>
+</section>
