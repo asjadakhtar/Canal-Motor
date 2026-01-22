95ea14275bbb206ece8f50a01cc259189d25d69c
asjadakhtar <asjadtech203@gmail.com>
2026-01-22 14:19:01 +0500

    #

--- templates/template-testing.php
+++ templates/template-testing.php
@@ -1,10 +1,10 @@
 <?php 
 /*
- * Template Name: Testing 
+ * Template Name: Container Template Canal
  */
 get_header(); ?>
 
-<main id="testing-template" class="<?php echo esc_attr(get_post_field('post_name', get_post())); ?>">
+<main id="container-template" class="<?php echo esc_attr(get_post_field('post_name', get_post())); ?>">
  <div id="fullpage">
      <div class="section h-screen bg-amber-400"></div>
      <div class="section h-screen bg-green-500"></div>
