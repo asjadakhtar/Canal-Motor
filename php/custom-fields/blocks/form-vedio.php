039a2158b7ac1dcf375101ca6a5db674115b721d
asjadakhtar <asjadtech203@gmail.com>
2026-01-24 13:41:49 +0500

    #

---
+++ php/custom-fields/blocks/form-vedio.php
@@ -0,0 +1,13 @@
+<section class="section font-primary">
+  <div class="relative w-full h-screen overflow-hidden bg-black">
+    <video 
+      src="http://localhost/canalmotor/wp-content/uploads/2026/01/1677561014_Product-CG-Video.mp4" 
+      class="w-full h-screen object-cover hidden md:block" 
+      autoplay 
+      muted 
+      loop 
+      playsinline
+    ></video>
+    <img src="http://localhost/canalmotor/wp-content/uploads/2026/01/x70plusform.png" alt="" class="w-full h-screen block md:hidden object-cover object-center">
+  </div>
+</section>
